837c5ae7f54081c40a598c9e01904cbf275bd8a2
mykenjoro <mykenjoroge01@gmail.com>
2022-07-25 23:32:50 +0300

    collection as an object during querying relationships

--- store/serializers.py
+++ store/serializers.py
@@ -1,15 +1,21 @@
 from decimal import Decimal
 from rest_framework import serializers
+from store.models import Product, Collection
 
-from store.models import Product
+
+class COllectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=200)
 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(decimal_places=2, max_digits=6,source= 'unit_price')
+    price = serializers.DecimalField(
+        decimal_places=2, max_digits=6, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
+    collection = COllectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
