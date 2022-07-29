61006479acf8358c11b5812fbb71cb9332b21d38
mykenjoro <mykenjoroge01@gmail.com>
2022-07-29 23:35:04 +0300

    Added class based views to collections

--- store/views.py
+++ store/views.py
@@ -25,19 +25,19 @@ class ProductList(APIView):
 
 class ProductDetail(APIView):
 
-    def get(self, request,id):
+    def get(self, request, id):
         product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
-    def put(self, request,id):
+    def put(self, request, id):
         product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def delete(self, request,id):
+    def delete(self, request, id):
         product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:
             return Response({'error': "Product cannot be deleted because it has been ordered"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
@@ -45,9 +45,8 @@ class ProductDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
+class CollectionList(APIView):
+    def get(self, request):
         queryset = Collection.objects.annotate(
             products_count=Count('product')
         ).all()
@@ -55,34 +54,43 @@ def collection_list(request):
             'request': request
         })
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = COllectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, id):
-
-    collection = get_object_or_404(
-        Collection.objects.annotate(
-            products_count=Count('products'), pk=id)
-    )
-    if request.method == 'GET':
-
+class CollectionDetail(APIView):
+    def get(self, request, id):
+        collection = get_object_or_404(
+            Collection.objects.annotate(
+                products_count=Count('products'), pk=id)
+        )
         serializer = COllectionSerializer(collection,)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, id):
+        collection = get_object_or_404(
+            Collection.objects.annotate(
+                products_count=Count('products'), pk=id)
+        )
         serializer = COllectionSerializer(
             collection,
             data=request.data,
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.metho == 'DELETE':
+    def delete(self, request, id):
+        collection = get_object_or_404(
+            Collection.objects.annotate(
+                products_count=Count('products'), pk=id)
+        )
         if collection.products.count() > 0:
             return Response({'error': 'Collection cannot be deleted as it already in use'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
