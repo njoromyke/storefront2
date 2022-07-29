901cdb4eae4bf411d00d9b4b696da3748da6b389
mykenjoro <mykenjoroge01@gmail.com>
2022-07-29 21:36:05 +0300

    deserializing data

--- store/views.py
+++ store/views.py
@@ -6,17 +6,22 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(
-        queryset, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        # serializer.validated_data
+        return Response("ok")
 
 
 @api_view()
-def product_detail(request, pk ):
-    product = get_object_or_404(Product, pk=pk )
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
 
