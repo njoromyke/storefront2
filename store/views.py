7b35ac5e5c1229e239fbe0ab893f1589519c1c2e
mykenjoro <mykenjoroge01@gmail.com>
2022-08-04 12:09:05 +0300

    searching

--- store/views.py
+++ store/views.py
@@ -1,14 +1,9 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.views import APIView
-
+from rest_framework.filters import SearchFilter
 from store.filters import ProdcutFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializers import Collection, CollectionSerializer, ProductSerializer, ReviewSerializer
@@ -18,8 +13,9 @@ from store import serializers
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,SearchFilter]
     filterset_class = ProdcutFilter
+    search_fields = ['title','description']
 
     def get_serializer_context(self):
         return {'request': self.request}
