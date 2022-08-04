e7e6b506505fe3850ac7aec2c070ffd2ebc9db8c
mykenjoro <mykenjoroge01@gmail.com>
2022-08-04 13:20:19 +0300

    pagination

--- store/views.py
+++ store/views.py
@@ -1,13 +1,17 @@
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter, OrderingFilter
-from store.filters import ProdcutFilter
-from .models import Collection, OrderItem, Product, Review
-from .serializers import Collection, CollectionSerializer, ProductSerializer, ReviewSerializer
+
 from store import serializers
+from store.filters import ProdcutFilter
+from store.pagination import DefaultPagination
+
+from .models import Collection, OrderItem, Product, Review
+from .serializers import (Collection, CollectionSerializer, ProductSerializer,
+                          ReviewSerializer)
 
 
 class ProductViewSet(ModelViewSet):
@@ -15,8 +19,9 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProdcutFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
-    ordering_fields = ['unit_price','last_update']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
