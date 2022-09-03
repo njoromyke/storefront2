8c129a3f4ace44c61329a46b83be62b9dce75530
njoromyke <mykenjoroge01@gmail.com>
2022-09-03 14:55:38 +0300

    Returning created order

--- store/views.py
+++ store/views.py
@@ -1,26 +1,26 @@
 
-from urllib import request
+from multiprocessing import context
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, DjangoModelPermissions
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
-                                   RetrieveModelMixin, UpdateModelMixin)
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from store.filters import ProdcutFilter
+from store.models import (Cart, CartItem, Collection, Customer, Order,
+                          OrderItem, Product, Review)
 from store.pagination import DefaultPagination
-from store.permissions import FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
-
-from store.models import (Cart, CartItem, Collection, Customer, Order, OrderItem, Product,
-                          Review)
+from store.permissions import IsAdminOrReadOnly, ViewCustomerHistoryPermission
 from store.serializers import (AddCartItemSerializer, CartItemSerializer,
-                               CartSerializer, Collection, CollectionSerializer, CreateOrderSerializer,
-                               CustomerSerializer, OrderSerializer, ProductSerializer,
-                               ReviewSerializer, UpdateCartItemSerializer)
+                               CartSerializer, Collection,
+                               CollectionSerializer, CreateOrderSerializer,
+                               CustomerSerializer, OrderSerializer,
+                               ProductSerializer, ReviewSerializer,
+                               UpdateCartItemSerializer)
 
 
 class ProductViewSet(ModelViewSet):
@@ -128,13 +128,19 @@ class CustomerViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(data=request.data, context={
+            'user_id': self.request.user.id
+        })
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return CreateOrderSerializer
         return OrderSerializer
-
-    def get_serializer_context(self):
-        return {'user_id': self.request.user.id}
 
     def get_queryset(self):
         user = self.request.user
