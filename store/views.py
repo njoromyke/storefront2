957865c9ef352febb494db03b0943e09059c3a13
njoromyke <mykenjoroge01@gmail.com>
2022-09-03 15:05:24 +0300

    revisitnfg permissions 73

--- store/views.py
+++ store/views.py
@@ -126,7 +126,13 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    http_method_names = [
+        'get', 'patch', 'delete', 'head', 'options']
+
+    def get_permissions(self):
+        if self.request.method in ['DELETE', 'PATCH']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
         serializer = CreateOrderSerializer(data=request.data, context={
